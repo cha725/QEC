6c9140c84a976c241a44bb8840f6574375de59dc
AustrianPenguin <charley.cummings@aol.com>
2025-12-08 14:09:24 +0000

    Add: function to add two rows together in a matrix.

--- binary_gaussian.py
+++ binary_gaussian.py
@@ -10,6 +10,7 @@ def row_swap(M: NDArray, i: int, j: int):
         - i (int): The index of the first row to be swapped.
         - j (int): The index of the second row to be swapped.
     """
+    M = M.copy()
     M[[i,j], :] = M[[j,i], :]
     return M
 
@@ -23,19 +24,38 @@ def scale_row(M: NDArray, i: int, c: int):
         - i (int): The index of the row to scale.
         - c (int): The constant to multiple the row with.
     """
+    M = M.copy()
     num_rows = M.shape[0]
     C = np.identity(num_rows)
     C[i][i] = c
     return np.matmul(C,M)
 
+def add_rows(M: NDArray, i: int, j: int):
+    """
+    Add two rows in the matrix.
+    Note: Add row j to row i. 
+    The new row i is i+j and row j remains untouched.
+
+    Parameters:
+        - M (NDArray): The matrix where we add to rows.
+        - i (int): The index of the row to add to. Row i will be row i + row j.
+        - j (int): The index of the row to add to row i.
+    """
+    M = M.copy()
+    M[[i],:] += M[[j],:]
+    return M
+
+
 
 if __name__ == "__main__":
     
     M = np.array([[1,2,3],[4,5,6]])
-
     print(row_swap(M,0,1))
-    print(scale_row(M,0,2))
-    
+    N = np.array([[1,2,3],[4,5,6]])
+    print(scale_row(N,0,2))
+    L = np.array([[1,2,3],[4,5,6]])
+    print(add_rows(L,0,1))
+
     
     
 
