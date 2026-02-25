1ce7173c4ce5d2c69da7a01be6f0ecf0a7155203
AustrianPenguin <charley.cummings@aol.com>
2026-02-25 21:49:24 +0000

    style: change stabiliser to stabilizer

--- CSScodes.py
+++ CSScodes.py
@@ -1,18 +1,18 @@
-from stabilisercodes import bit, Stabiliser, StabiliserCode
+from stabilizercodes import bit, Stabilizer, StabilizerCode
 
-class CSSStabiliser(Stabiliser):
+class CSSStabilizer(Stabilizer):
     """
-    Represents a stabiliser in a CSS code.
-    A stabiliser in a CSS code has phase=1 and either contains:
+    Represents a stabilizer in a CSS code.
+    A stabilizer in a CSS code has phase=1 and either contains:
         - only I and Z operators; or
         - only I and X operators.
-        e.g.    Tensor(X,X,I), Tensor(Z,I,Z,Z) are CSS stabilisers.
-                Tensor(X,I,Z) is not a CSS stabiliser.
+        e.g.    Tensor(X,X,I), Tensor(Z,I,Z,Z) are CSS stabilizers.
+                Tensor(X,I,Z) is not a CSS stabilizer.
 
     Attributes:
-        z_type (bool):  True if stabiliser contains only Z and I.
-                        False if stabiliser contains only X and I.
-        vec (list[int]): Exponent vector of the stabiliser.
+        z_type (bool):  True if stabilizer contains only Z and I.
+                        False if stabilizer contains only X and I.
+        vec (list[int]): Exponent vector of the stabilizer.
     """
 
     def __init__(self,
@@ -28,7 +28,7 @@ class CSSStabiliser(Stabiliser):
             z_vec : list[bit] = [0]*len(x_vec)
         super().__init__(z_vec, x_vec, phase=1)
 
-class CSSStabiliserCode(StabiliserCode):
+class CSSStabilizerCode(StabilizerCode):
     def __init__(self,
                  z_vecs: list[list[bit]] | None = None,
                  x_vecs: list[list[bit]] | None = None):
@@ -36,10 +36,10 @@ class CSSStabiliserCode(StabiliserCode):
             z_vecs = []
         if x_vecs is None:
             x_vecs = []
-        z_stabilisers = [CSSStabiliser(True, vec) for vec in z_vecs]
-        x_stabilisers = [CSSStabiliser(False, vec) for vec in x_vecs]
-        all_stabilisers = z_stabilisers + x_stabilisers
-        super().__init__(all_stabilisers)
+        z_stabilizers = [CSSStabilizer(True, vec) for vec in z_vecs]
+        x_stabilizers = [CSSStabilizer(False, vec) for vec in x_vecs]
+        all_stabilizers = z_stabilizers + x_stabilizers
+        super().__init__(all_stabilizers)
 
 
 
